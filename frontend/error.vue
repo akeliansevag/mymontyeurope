087ab4651604a84af8b6ee2c9af3c997cc96204f
Hassan Al Jammal <hassan-jammal@hotmail.com>
2024-11-18 12:44:45 +0200

    .

--- frontend/error.vue
+++ frontend/error.vue
@@ -28,19 +28,22 @@
 <script setup lang="ts">
 	import { useLocale } from './composables/useLocale'; 
 	import type { NuxtError } from '#app'
-	
+
 	const { t } = useLocale(); 
 
 	const props = defineProps({
 		error: Object as () => NuxtError
 	})
 
-	useSeoMeta({
+	useServerSeoMeta({
 		title: () => t('Pages.Error.Seo.Title', { code: props.error.statusCode || 'Unknown' }),
 		description: () => t('Pages.Error.Seo.Description', { message: props.error.message || t('Pages.Error.Seo.Default Message') }),
 
 		ogTitle: () => t('Pages.Error.Seo.Og Title', { code: props.error.statusCode || 'Unknown' }),
 		ogDescription: () => t('Pages.Error.Seo.Og Description', { message: props.error.message || t('Pages.Error.Seo.Default Message') }),
+
+		twitterTitle: () => t('Pages.Error.Seo.Og Title', { code: props.error.statusCode || 'Unknown' }),
+        twitterDescription: () => t('Pages.Error.Seo.Og Description', { message: props.error.message || t('Pages.Error.Seo.Default Message') }),
 	})
 
 	// const handleError = () => clearError({ redirect: '/' })
