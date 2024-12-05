7fb72a69a57306eca0ae3b3f1c10c7737007b341
Hassan Al Jammal <hassan-jammal@hotmail.com>
2024-12-05 09:41:59 +0200

    .

--- frontend/components/AppFooter.vue
+++ frontend/components/AppFooter.vue
@@ -9,7 +9,7 @@
                     <div v-if="errors.newsletter_email" class="text-xs text-red-500">{{ errors.newsletter_email }}</div>
                     <div v-if="submissionMessage" class="text-xs text-gray-500">{{ submissionMessage }}</div>
                     <button @click.prevent="handleSubmit" class="py-2 px-4 text-base text-white hover:text-black bg-primary hover:bg-white rounded-[30px] select-none font-AeonikMedium transition-all duration-300 ease-in-out">{{ t(`General.Buttons.Subscribe`) }}</button>
-                    <p class="text-[#D4D4D4] text-sm">By subscribing to our newsletter, you acknowledge that you have read and accepted our Terms & Conditions; read and understood our Privacy Notice.</p>
+                    <p class="text-[#727272] text-sm">By subscribing to our newsletter, you acknowledge that you have read and accepted our Terms & Conditions; read and understood our Privacy Notice.</p>
                 </div>
                 <ul class="lg:col-span-2 grid sm:grid-cols-4 sm:justify-between gap-6 sm:gap-3">
                     <li v-for="(item, index) in menuItems" :key="index" :class="{ 'active': activeIndices.includes(index) }" @click="toggleActive(index)" class="flex flex-col sm:gap-10 group">
@@ -37,7 +37,7 @@
                 </ul>
             </div>
 
-            <hr class="border-[#D4D4D422]" />
+            <hr class="border-[#72727222]" />
 
             <div class="grid grid-cols-1 md:grid-cols-2 justify-between gap-8 md:gap-4 py-6 md:py-12 max-md:text-center">
                 <div class="flex flex-col gap-2">
@@ -58,12 +58,12 @@
                             <img src="/images/icons/youtube.svg" alt="Youtube" width="24" height="24" />
                         </a>
                     </div>
-                    <p class="text-[#D4D4D4] text-sm mt-2">{{ t(`Footer.Toll-Free Number`) }}: <a href="tel:+448082387522" class="underline underline-offset-4">+44 808 238 7522</a></p>
-                    <p class="text-[#D4D4D4] text-sm"><a href="mailto:support@mymonty.com" class="underline underline-offset-4">{{ t(`Footer.Email Us`) }}</a></p>
+                    <p class="text-[#727272] text-sm mt-2">{{ t(`Footer.Toll-Free Number`) }}: <a href="tel:+448082387522" class="underline underline-offset-4">+44 808 238 7522</a></p>
+                    <p class="text-[#727272] text-sm"><a href="mailto:support@mymonty.com" class="underline underline-offset-4">{{ t(`Footer.Email Us`) }}</a></p>
                 </div>
 
-                <div class="flex flex-col gap-2 md:ml-auto text-[#D4D4D4]">
-                    <div class="flex max-md:justify-center items-center gap-6 md:ml-auto text-[#D4D4D4]">
+                <div class="flex flex-col gap-2 md:ml-auto text-[#727272]">
+                    <div class="flex max-md:justify-center items-center gap-6 md:ml-auto text-[#727272]">
                         <NuxtLinkLocale :to="'/terms-and-conditions'" class="text-sm underline underline-offset-4 hover:text-primary transition-all duration-300 ease-in-out">{{ t(`General.Links.Terms & Conditions`) }}</NuxtLinkLocale>
                         <NuxtLinkLocale :to="'/privacy-policy'" class="text-sm underline underline-offset-4 hover:text-primary transition-all duration-300 ease-in-out">{{ t(`General.Links.Privacy Policy`) }}</NuxtLinkLocale>
                         <NuxtLinkLocale :to="'/cookie-policy'" class="text-sm underline underline-offset-4 hover:text-primary transition-all duration-300 ease-in-out">{{ t(`General.Links.Cookie Policy`) }}</NuxtLinkLocale>
@@ -71,18 +71,18 @@
                             {{ t(`General.Links.Cookie Settings`) }}
                         </a>
                     </div>
-                    <div class="flex max-md:justify-center items-center gap-6 text-[#D4D4D4]">
+                    <div class="flex max-md:justify-center items-center gap-6 text-[#727272]">
                         <NuxtLinkLocale :to="'/paynetics-terms-and-conditions'" class="text-sm underline underline-offset-4 hover:text-primary transition-all duration-300 ease-in-out">{{ t(`General.Links.Paynetics Terms & Conditions`) }}</NuxtLinkLocale>
                         <NuxtLinkLocale :to="'/paynetics-privacy-policy'" class="text-sm underline underline-offset-4 hover:text-primary transition-all duration-300 ease-in-out">{{ t(`General.Links.Paynetics Privacy Policy`) }}</NuxtLinkLocale>
                     </div>
                 </div>
             </div>
 
-            <hr class="border-[#D4D4D422]" />
+            <hr class="border-[#72727222]" />
 
             <div class="flex flex-col gap-6 py-6 sm:py-12">
-                <p class="text-base text-[#D4D4D4]" v-html="t(`Footer.Copyright`)"></p>
-                <p class="text-[#D4D4D4] text-sm mt-2" v-html="t(`Footer.Paynetics Agreement`)"></p>
+                <p class="text-base text-[#727272]" v-html="t(`Footer.Copyright`)"></p>
+                <p class="text-[#727272] text-sm mt-2" v-html="t(`Footer.Paynetics Agreement`)"></p>
             </div>
         </div>
     </footer>
